c5daf0c2115b112e457af106f564a58cecf39d32
Ederson <edersonrp@gmail.com>
2020-06-03 21:34:25 -0300

    Acerto da função para buscar cidades

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -1,22 +1,37 @@
 function populateUfs() {
   const ufSelect = document.querySelector("select[name=uf]");
   fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
-    .then(res => res.json())
-    .then(states => {
-      for(state of states) {
-        ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
+    .then((res) => res.json())
+    .then((states) => {
+      for (state of states) {
+        ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`;
       }
-     
     });
 }
 
-populateUfs()
+populateUfs();
 
 function getCities(event) {
-  const citySelect = document.querySelector("select[name=city]")
-  console.log(event.target.value);
-  const ufValue = event.target.value
-  const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/33/municipios`
+  const citySelect = document.querySelector("select[name=city]");
+  const stateInput = document.querySelector("input[name=state]");
+
+  const ufValue = event.target.value;
+  const indexOfSelectStatate = event.target.selectedIndex
+  stateInput.value = event.target.options[indexOfSelectStatate].text
+
+  const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
+  citySelect.innerHTML = ""
+  citySelect.disabled = true
+  fetch(url)
+    .then((res) => res.json())
+    .then((cities) => {
+      
+      for ( const city of cities) {
+        citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`;
+      }
+      citySelect.disabled = false
+    })
 }
 
-document.querySelector("select[name=uf]").addEventListener("change", getCities)
+
+document.querySelector("select[name=uf]").addEventListener("change", getCities);
